f25722173776f9a2f200d97ad867f41a2c339fa1
Ike-G <07iglassbrook@brightoncollege.net>
2021-11-06 00:05:40 +0000

    finished webpage

--- app.js
+++ app.js
@@ -7,8 +7,13 @@ const port = 3000;
 let answers = JSON.parse(fs.readFileSync("serverData.json"));
 
 app.use(express.static('static'));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+app.get('/serverData.json', (req, res) => {
+    res.json(answers);
 });
 
 app.listen(port, () => {
--- static/script.js
+++ static/script.js
@@ -1,26 +1,42 @@
 const clientData = JSON.parse($("#clientData").html());
 
+$(".accordion").on("click", (event) => {
+    event.target.classList.toggle('inactive');
+    event.target.classList.toggle('active');
+    var panel = event.target.nextElementSibling;
+    if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+    } else {
+        panel.style.maxHeight = panel.scrollHeight+"px";
+    }
+});
+
 $(".question form").on("submit", (event) => {
     let question = event.target.parentElement;
-    let n = Array.from(question.parentElement.children).indexOf(question);
+    let n = Array.from(question.parentElement.parentElement.children).indexOf(question.parentElement);
     let response = $(event.target).serializeArray();
-    console.log(response);
-    let answer;
+    $(event.target).children().each((index, element) => {
+        $(element.firstElementChild).prop("disabled", true);
+    });
+    $(event.target.lastElementChild).css("display", "none");
     // Some data is server-side
     if (n >= clientData.length) {
-        fetch('serverData.json').then((res) => {
-            res.json().then((data) => {
-                answer = data[n-clientData.length];
+        fetch('serverData.json')
+            .then(res => res.json())
+            .then(data => {
+                if (_.isEqual(data[n-clientData.length], response)) 
+                    $(question.nextElementSibling).fadeIn();
+                else 
+                    $(question.nextElementSibling.nextElementSibling).fadeIn();
+                console.log(response);
             });
-        });
     } else { // Other data is client-side
-        answer = clientData[n];
+        console.log(response);
+        if (_.isEqual(clientData[n], response)) {
+            $(question.nextElementSibling).fadeIn();
+        } else {
+            $(question.nextElementSibling.nextElementSibling).fadeIn();
+        }
     }
-    if (response == answer) {
-        $(question.nextElementSibling).css("display", "block");
-    } else {
-        $(question.nextElementSibling.nextElementSibling).css("display", "block");
-    }
-
     event.preventDefault();
 });
